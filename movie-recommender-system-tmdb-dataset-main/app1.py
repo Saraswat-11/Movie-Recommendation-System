1872f33a0c6454a7713b3d1f6151db1fc2c59a16
Saraswat-11 <161245663+Saraswat-11@users.noreply.github.com>
2024-03-30 21:56:06 +0530

    Update app1.py

--- movie-recommender-system-tmdb-dataset-main/app1.py
+++ movie-recommender-system-tmdb-dataset-main/app1.py
@@ -25,8 +25,8 @@ def recommend(movie):
 
 
 st.header('Movie Recommender System')
-movies = pickle.load(open('model/movie_list.pkl','rb'))
-similarity = pickle.load(open('model/similarity.pkl','rb'))
+movies = pickle.load(open('OneDrive/movie-recommender-system-tmdb-dataset-main/movie_list.pkl','rb'))
+similarity = pickle.load(open('OneDrive/movie-recommender-system-tmdb-dataset-main/similarity.pkl','rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
